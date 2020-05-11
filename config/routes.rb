bdfeaf7db1b4cc59389d436c9e7f44b18f5d8002
dwaynediddy <burrows.dwayne@icloud.com>
2020-05-11 13:08:21 +1000

    heroku debug attempt 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :marketplaces
   root to: 'pages#home'
   get "/marketplaces", to: 'marketplace#index' as: marketplaces
-  devise_for :views
+  # devise_for :views
   devise_for :patrons
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
