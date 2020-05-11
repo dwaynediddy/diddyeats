d24713da9bffebac9a213874c0048280e17d227c
dwaynediddy <burrows.dwayne@icloud.com>
2020-05-11 15:31:39 +1000

    deployment attempt 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :marketplaces
-  root to: 'pages#home'
-  get "/marketplaces", to: 'marketplaces#index', as: "marketplaces"
+  root 'pages#home'
+
   devise_for :views
   devise_for :patrons
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
